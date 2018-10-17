786a5975bab24fe3f8e6c09e627e5535b74f6412
Wilgert Velinga <wilgert.velinga@nl.abnamro.com>
2018-10-17 15:53:11 +0200

    provide custom CDK_DRAG_CONFIG and set dragStartTreshold to 0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {DragDropModule} from '@angular/cdk/drag-drop';
+import {CDK_DRAG_CONFIG, DragDropModule} from '@angular/cdk/drag-drop';
 import {DragAndDropDemoComponent} from './drag-drop/drag-drop-demo';
 
 @NgModule({
@@ -14,7 +14,12 @@ import {DragAndDropDemoComponent} from './drag-drop/drag-drop-demo';
     BrowserModule,
     DragDropModule
   ],
-  providers: [],
+  providers: [
+    {provide: CDK_DRAG_CONFIG, useValue: {
+        dragStartThreshold: 0,
+        pointerDirectionChangeThreshold: 5
+      }}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
