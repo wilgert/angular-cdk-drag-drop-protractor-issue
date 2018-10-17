950db487c673bbdac13c2678ec355b42c9cd3194
Wilgert Velinga <wilgert.velinga@nl.abnamro.com>
2018-10-17 16:37:20 +0200

    remove custom CDK_DRAG_CONFIG

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {CDK_DRAG_CONFIG, DragDropModule} from '@angular/cdk/drag-drop';
+import { DragDropModule} from '@angular/cdk/drag-drop';
 import {DragAndDropDemoComponent} from './drag-drop/drag-drop-demo';
 
 @NgModule({
@@ -14,12 +14,7 @@ import {DragAndDropDemoComponent} from './drag-drop/drag-drop-demo';
     BrowserModule,
     DragDropModule
   ],
-  providers: [
-    {provide: CDK_DRAG_CONFIG, useValue: {
-        dragStartThreshold: 0,
-        pointerDirectionChangeThreshold: 5
-      }}
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
